9259aa8267abe9a29425d1c27d12129a2f91ae2d
Vinicius Santos <viniciusantos.florencio@gmail.com>
2022-06-07 21:32:35 -0300

    Refatora código

--- src/Models/User.ts
+++ src/Models/User.ts
@@ -1,22 +1,4 @@
-import mongoose, { Schema } from 'mongoose'
+import mongoose from 'mongoose'
 
-class User {
-  static init() {
-    return new Schema({
-      name: String,
-      email: String,
-      password: String,
-      posts: [
-        {
-          title: String,
-          description: String,
-          likes: Number,
-          date: Date,
-        },
-      ],
-    })
-  }
-}
-
-const user = User.init()
-export const UserModel = mongoose.model('User', user)
+class User extends mongoose.Schema {}
+export const UserModel = mongoose.model('User', new User())
