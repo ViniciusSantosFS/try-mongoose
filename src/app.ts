e1d171994b0427a124e4aef4b0360c5f03ce7075
Vinicius Santos <viniciusantos.florencio@gmail.com>
2022-06-10 17:29:06 -0300

    Adiciona rota para criar uma venda

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,10 @@
 import express, { ErrorRequestHandler } from 'express'
+import { saleController } from './controller/sale-controller'
 
 const app = express()
 app.use(express.json())
+
+app.post('/sale', saleController.create)
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
   return res.status(500).end({ message: err.message })
