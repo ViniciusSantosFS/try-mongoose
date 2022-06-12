c794f60693a13e976bb47a880d69e780701fee19
Vinicius Santos <viniciusantos.florencio@gmail.com>
2022-06-12 16:25:14 -0300

    Ajusta rotas

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,13 @@ import { saleController } from './controller/sale-controller'
 const app = express()
 app.use(express.json())
 
-app.post('/sale', saleController.create)
+app.get('/sales', saleController.index)
+app.post('/sales', saleController.store)
+app.put('/sales/:id', saleController.update)
+app.delete('/sales/:id', saleController.destroy)
 
 const errorHandler: ErrorRequestHandler = (err, req, res, next) => {
-  return res.status(500).end({ message: err.message })
+  return res.status(500).json({ message: err.message })
 }
 app.use(errorHandler)
 
